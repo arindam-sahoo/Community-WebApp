a136c2873d1a96875d9214f5483b0714594accb9
Arindam Sahoo <arindams2002@gmail.com>
2024-01-17 15:35:37 +0530

    Models Created

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,29 +1,27 @@
 from django.shortcuts import render
+from .models import Room
 
-rooms = [
-    {
-        'id': 1,
-        'name': 'Let\'s Test out Comms-ee'
-    },
-    {
-        'id': 2,
-        'name': 'Building the server with Python-Django'
-    },
-    {
-        'id': 3,
-        'name': 'Using HTML Templating now, gonna change to React later'
-    }
-]
+# rooms = [
+#     {
+#         'id': 1,
+#         'name': 'Let\'s Test out Comms-ee'
+#     },
+#     {
+#         'id': 2,
+#         'name': 'Building the server with Python-Django'
+#     },
+#     {
+#         'id': 3,
+#         'name': 'Using HTML Templating now, gonna change to React later'
+#     }
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'api/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-    
+    room = Room.objects.get(id=pk)    
     context = {'room': room}
     return render(request, 'api/room.html', context)
