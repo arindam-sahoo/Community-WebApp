84b8b595140c2c3695b773b48ed9cc410f287b5e
Arindam Sahoo <arindams2002@gmail.com>
2024-01-18 01:57:39 +0530

    Search Functionality Added

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,25 +1,20 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from django.db.models import Q
+from .models import Room, Topic
 from .forms import RoomForm
 
-# rooms = [
-#     {
-#         'id': 1,
-#         'name': 'Let\'s Test out Comms-ee'
-#     },
-#     {
-#         'id': 2,
-#         'name': 'Building the server with Python-Django'
-#     },
-#     {
-#         'id': 3,
-#         'name': 'Using HTML Templating now, gonna change to React later'
-#     }
-# ]
-
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    q = request.GET.get('q') if request.GET.get('q')!=None else ''
+
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        )
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
     return render(request, 'api/home.html', context)
 
 def room(request, pk):
