5e812b5f203b5e0efe86ae88a5ee295d8d7c35e9
Arindam Sahoo <arindams2002@gmail.com>
2024-01-17 22:59:23 +0530

    CRUD Functionalities Added

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import home, room
+from django.urls import path
+from .views import home, room, create_room, update_room, delete_room
 
 urlpatterns = [
     path('', home, name='homepage'),
     path('room/<str:pk>', room, name='room'),
+    path('create-room/', create_room, name='create-room'),
+    path('update-room/<str:pk>/', update_room, name='update-room'),
+    path('delete-room/<str:pk>/', delete_room, name='delete-room'),
 ]
